42ac496e27b7a29e4a4d50c5d0bc145edb2b31fb
Fabian Ladenstein <36959878+schnider94@users.noreply.github.com>
2023-01-07 12:07:23 +0100

    add mongodb in k8s

--- microservices/user-service/config.js
+++ microservices/user-service/config.js
@@ -2,12 +2,17 @@ const mongoose = require('mongoose');
 
 const setupDatabase = function() {
     // DATABASE
-    // TODO: Use environment variables from Helm
-    mongoose.connect("mongodb://127.0.0.1:27017/passport-jwt", {
+    const host = process.env.DB_HOST;
+    const username = process.env.DB_USERNAME;
+    const password = process.env.DB_PASSWORD;
+    const database = process.env.DB_NAME;
+
+    const connectString = `mongodb://${username}:${password}@${host}/${database}`;
+
+    mongoose.connect(connectString, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
-    mongoose.set("useCreateIndex", true);
     mongoose.connection.on('error', error => console.log(error) );
     mongoose.Promise = global.Promise;
 }
