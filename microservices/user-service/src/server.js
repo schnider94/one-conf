b9cd3ea656a15dae7f8bc8b05ccefb68e818b59b
Fabian Ladenstein <36959878+schnider94@users.noreply.github.com>
2023-01-07 10:11:47 +0100

    use published npm packages

--- microservices/user-service/src/server.js
+++ microservices/user-service/src/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const passport = require('passport');
 const bodyParser = require('body-parser');
 const auth = require('./auth');
-const jwtMiddleware = require('@one-conf/jwt-middleware');
+const jwtMiddleware = require('@schnider94/jwt-middleware');
 
 auth.setupPassport();
 
--- scripts/docker/login.sh
+++
@@ -1,2 +0,0 @@
-CR_PAT=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
-echo $CR_PAT | docker login ghcr.io -u schnider94 --password-stdin
