2bede7b8755fbd468d3623f35af817c08da7de66
Fabian Ladenstein <36959878+schnider94@users.noreply.github.com>
2023-02-22 12:51:32 +0100

    app fixes

--- app/src/plugins/axios.js
+++ app/src/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
 export default () => {
-    axios.defaults.baseURL = 'http://192.168.178.30/api'; // 'http://www.schnider.io/api';
+    axios.defaults.baseURL = 'http://www.schnider.io/api';
     axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 }
